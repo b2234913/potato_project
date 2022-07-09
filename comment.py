183b9e54447131767747953f44a769c6873d0ffc
victor <b2234913@gmail.com>
2022-07-09 12:17:01 +0800

    add comment.py and update something

--- comment.py
+++ comment.py
@@ -2,9 +2,7 @@ from utils.potato_service import PotatoService
 from config import config, users
 # os.system("pkill -9 Google Chrome")
 
-
-
 PotatoService = PotatoService()
 PotatoService.login(users.info['ShooterIsMe'], config.login_url)
 uuid_list = PotatoService.get_latest_post_uuid_list(config.latest_post_url, 30)
-PotatoService.like_post(uuid_list, False)
+PotatoService.comment_post(uuid_list, True)
